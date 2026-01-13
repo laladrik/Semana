b9e3de65011b47308ea1f3b6488a7e85e01f615d
Aleksei Markov <laladrik@pm.me>
2026-01-13 17:49:47 +0000

    feat(calendar): proper date arithmetic
    The libc functions to calculate the dates are removed.  The algorithm of
    Neri-Schneider is used instead of them.

--- crates/calendar/src/date.rs
+++ crates/calendar/src/date.rs
@@ -59,12 +59,12 @@ impl Ord for Date {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         let year_ordering = self.year.cmp(&other.year);
         if year_ordering.is_ne() {
-            return year_ordering
+            return year_ordering;
         }
 
         let month_ordering = self.month.cmp(&other.month);
         if month_ordering.is_ne() {
-            return month_ordering
+            return month_ordering;
         }
 
         self.day.cmp(&other.day)
@@ -232,6 +232,41 @@ impl DateString {
 }
 
 impl Date {
+    #[cfg(test)]
+    const fn new<const YEAR: u16, const MONTH: u8, const DAY: u8>() -> Date {
+        const {
+            struct C<const YEAR: u16, const MONTH: u8>();
+            impl<const YEAR: u16, const MONTH: u8> C<YEAR, MONTH> {
+                const DAY_CAP: u8 = Date::month_day_count(YEAR, MONTH);
+            }
+
+            assert!(MONTH > 0);
+            assert!(MONTH < 13);
+            assert!(DAY > 0);
+            assert!(DAY <= C::<YEAR, MONTH>::DAY_CAP);
+        };
+
+        Date {
+            year: YEAR,
+            month: MONTH,
+            day: DAY,
+        }
+    }
+
+    pub const fn month_day_count(year: u16, month: u8) -> u8 {
+        match month {
+            2 => {
+                if Self::is_leap_year(year) {
+                    29
+                } else {
+                    28
+                }
+            }
+            4 | 6 | 9 | 11 => 30,
+            _ => 31,
+        }
+    }
+
     /// return the byte representation of the date.
     pub const fn iso_8601(&self) -> DateString {
         DateString([
@@ -249,32 +284,24 @@ impl Date {
     }
 
     pub fn add_week(&self) -> Date {
-        add_days(self, 7)
+        Self::add_days(self, 7)
     }
 
     pub fn subtract_week(&self) -> Date {
-        add_days(self, -7)
+        Self::add_days(self, -7)
     }
 
     pub fn add_days(&self, days: i16) -> Date {
-        add_days(self, days)
-    }
-
-    pub const fn month_day_count(year: u16, month: u8) -> u8 {
-        match month {
-            2 => {
-                if Self::is_leap_year(year) {
-                    29
-                } else {
-                    28
-                }
-            }
-            4 | 6 | 9 | 11 => 30,
-            _ => 31,
-        }
+        let total_days = Self::calculate_total_days(self);
+        let n: i32 = total_days + days as i32;
+        eafs::calculate_gregorian_date(n)
     }
 
     pub fn try_new(year: u16, month: u8, day: u8) -> Result<Date, InvalidInput> {
+        if year < 1 {
+            return Err(InvalidInput);
+        }
+
         if !(month > 0 && month <= 12) {
             return Err(InvalidInput);
         }
@@ -292,128 +319,123 @@ impl Date {
         year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
     }
 
-    pub fn days_from_epoch(&self) -> i32 {
-        // Days from months (approximate).  the 31 from December is skipped, because when we pass
-        // December we pass the year.  Given that the days are in `year_days` already.
-        let month_capacities = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30];
-        let month_days: i32 = month_capacities.iter().take(self.month as usize - 1).sum();
+    pub fn from_days(days: i32) -> Self {
+        assert!(days >= 0);
+        eafs::calculate_gregorian_date(days)
+    }
 
-        let year_days = years_to_days(self.year);
-        let total_days = self.day as i32 + month_days + year_days;
-
-        // Adjust for leap years in current year
-        if self.month > 2 && Self::is_leap_year(self.year) {
-            total_days + 1
-        } else {
-            total_days
-        }
+    /// Returns the number of the days which have past before the Date.  Given that, 0001-01-01
+    pub fn calculate_total_days(&self) -> i32 {
+        eafs::calculate_rata_die_from_gregorian_calendar(self)
     }
 
     pub fn subtract(&self, other: &Date) -> i32 {
-        let self_days = self.days_from_epoch();
-        let other_days = other.days_from_epoch();
+        let self_days = self.calculate_total_days();
+        let other_days = other.calculate_total_days();
         self_days - other_days
     }
 }
 
-fn years_to_days(year: u16) -> i32 {
-    const START: i32 = 1970;
-    let years_since_the_start: i32 = (year as i32) - START;
-    // As 1970 is not a leap year, that fact that the difference with it is divisible by 4 is an
-    // error.  Therefore, we do (+2) and cound from 1968.
-    //
-    // As we count the days in the years before `year` we do -1
-    let leap_years = years_since_the_start + 2 - 1;
-    years_since_the_start
-        * 365
-        + leap_years / 4
-        - (leap_years / 100)
-        + leap_years / 400
-}
+mod eafs {
+    //! Euclidean affine functions and their application to calendar algorithms DOI:
+    //! 10.0002/spe.3172 Neri-Schneider
 
-use std::ffi::c_char;
-use std::ffi::c_int;
+    use super::Date;
+    pub fn calculate_gregorian_date(rata_die: i32) -> Date {
+        assert!(rata_die >= 0);
+        let n1 = 4 * rata_die + 3;
 
-#[allow(non_camel_case_types)]
-#[repr(C)]
-struct c_tm {
-    /// Seconds          [0, 60]
-    tm_sec: c_int,
-    /// Minutes          [0, 59]
-    tm_min: c_int,
-    /// Hour             [0, 23]
-    tm_hour: c_int,
-    /// Day of the month [1, 31]
-    tm_mday: c_int,
-    /// Month            [0, 11]  (January = 0)
-    tm_mon: c_int,
-    /// Year minus 1900
-    tm_year: c_int,
-    /// Day of the week  [0, 6]   (Sunday = 0)
-    tm_wday: c_int,
-    /// Day of the year  [0, 365] (Jan/01 = 0)
-    tm_yday: c_int,
-    /// Daylight savings flag
-    tm_isdst: c_int,
-    /// Seconds East of UTC
-    tm_gmtoff: c_long,
-    /// Timezone abbreviation
-    tm_zone: *mut c_char,
-}
+        let n2 = 3 + 4 * {
+            // nc
+            n1 % 146097 / 4
+        };
 
-const TM_YEAR_SHIFT: i16 = -1900;
-const TM_MONTH_SHIFT: i16 = -1;
+        let n3 = 461
+            + 5 * {
+                // ny
+                n2 % 1461 / 4
+            };
 
-#[allow(non_camel_case_types)]
-type c_time_t = u64;
+        let m = n3 / 153;
 
-#[link(name = "c")]
-unsafe extern "C" {
-    /// out is nullable
-    fn time(out: *mut c_time_t) -> c_time_t;
-    fn localtime(time: *const c_time_t) -> *mut c_tm;
-    fn localtime_r(time: *const c_time_t, result: *mut c_tm) -> *mut c_tm;
-    /// c_tm::tm_yday and c_tm::tm_wday are ignored.  Reference: ctime(3)
-    fn mktime(broken_time: *const c_tm) -> c_time_t;
-}
+        let j = if m >= 13 { 1 } else { 0 };
+        let year = j + {
+            let c = n1 / 146097; //146097 the number of days in 400 years
+            // y
+            100 * c + {
+                // z
+                n2 / 1461
+            }
+        };
 
-fn add_days(from: &Date, days: i16) -> Date {
-    // SAFETY: localtime can't fail with the current time.  Reference: ctime(3)
-    unsafe {
-        let now_seconds: c_time_t = time(std::ptr::null_mut());
-        let mut now_broken: c_tm = std::mem::zeroed();
-        let ret: *const _ = localtime_r(&now_seconds, &mut now_broken);
-        if ret.is_null() {
-            panic!("we can't get the today's date");
+        let month = m - 12 * j;
+        let day = 1 + {
+            // d
+            n3 % 153 / 5
+        };
+
+        assert!(year >= 0);
+        assert!(month >= 0);
+        assert!(day >= 0);
+        assert!(year <= u16::MAX as i32);
+        assert!(month <= u8::MAX as i32);
+        assert!(day <= u8::MAX as i32);
+        Date::try_new(year as u16, month as u8, day as u8)
+            .expect("Failed to calculate a date with Neri-Shneider")
+    }
+
+    pub fn calculate_rata_die_from_gregorian_calendar(date: &Date) -> i32 {
+        let Date { year, month, day } = date;
+        let j = if *month <= 2 { 1 } else { 0 };
+        // underflow safety: the structure Date guarantees that year is positive
+        let y = *year - j;
+        let m = *month as u16 + 12 * j;
+        // underflow safety: the structure Date guarantees that day is positive
+        let d = day - 1;
+        let c = y / 100;
+
+        let ystar = 1461 * (y as i32) / 4 - (c as i32) + (c as i32) / 4;
+        // might go negative
+        let mstar = (153 * (m as i32) - 457) / 5;
+        let ret = ystar + mstar + d as i32;
+        assert!(ret >= 0);
+        ret
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+        #[test]
+        fn test_roundtrip() {
+            for y in 1..=2100 {
+                for m in 1..=12 {
+                    for d in 1..=31 {
+                        if let Ok(input_date) = Date::try_new(y, m, d) {
+                            let rata_die = calculate_rata_die_from_gregorian_calendar(&input_date);
+                            let date = calculate_gregorian_date(rata_die);
+                            assert_eq!(input_date, date);
+                        }
+                    }
+                }
+            }
         }
 
-        now_broken.tm_year = (from.year as i32 + TM_YEAR_SHIFT as i32) as _;
-        now_broken.tm_mon = (from.month as i32 + TM_MONTH_SHIFT as i32) as _;
-        now_broken.tm_mday = from.day as _;
-        let from_time_seconds: c_time_t = mktime(&now_broken as _);
-
-        let diff = days as i64 * SECONDS_PER_DAY as i64;
-        let result_seconds: c_time_t = if diff > 0 {
-            from_time_seconds + diff as u64
-        } else {
-            from_time_seconds - diff.abs() as u64
-        };
-
-        let result_broken: *const c_tm = localtime(&result_seconds as _);
-        let year = (*result_broken).tm_year as i32 - TM_YEAR_SHIFT as i32;
-        assert!(year > 0 && year <= u16::MAX as i32);
-        let month = (*result_broken).tm_mon - TM_MONTH_SHIFT as i32;
-        assert!(month > 0 && month < u8::MAX as i32);
-
-        let ret = Date {
-            year: year as u16,
-            month: month as u8,
-            day: (*result_broken).tm_mday as _,
-        };
-
-        let ret_days = ret.subtract(from);
-        assert_eq!(ret_days, days.into(), "the result date is wrong: {:?}", ret);
-        ret
+        #[test]
+        fn test_invariant() {
+            let mut last_rata_die: Option<i32> = None;
+            for y in 1..=2100 {
+                for m in 1..=12 {
+                    for d in 1..=31 {
+                        if let Ok(input_date) = Date::try_new(y, m, d) {
+                            let rata_die = calculate_rata_die_from_gregorian_calendar(&input_date);
+                            if let Some(x) = last_rata_die.replace(rata_die) {
+                                assert_eq!(x + 1, rata_die);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -421,68 +443,66 @@ fn add_days(from: &Date, days: i16) -> Date {
 mod tests {
     use super::*;
 
-    mod years_to_days {
+    mod date_add_days {
         use super::*;
-        #[test]
-        fn test_the_first_leap_year() {
-            let days = years_to_days(1972);
-            assert_eq!(days, 365 * 2);
+
+        const fn newdate<const YEAR: u16, const MONTH: u8, const DAY: u8>() -> Date {
+            Date::new::<YEAR, MONTH, DAY>()
         }
 
         #[test]
-        fn test_the_year_after_the_first_leap_year() {
-            let days = years_to_days(1973);
-            assert_eq!(days, 365 * 3 + 1);
+        fn test_regular_year() {
+            let inputs = [
+                (newdate::<2001, 1, 1>(), newdate::<2001, 1, 8>(), 7),
+                (newdate::<2000, 2, 28>(), newdate::<2000, 3, 6>(), 7),
+                (newdate::<2001, 2, 28>(), newdate::<2001, 3, 7>(), 7),
+            ];
+
+            for (i, (st, expected_date, days)) in inputs.iter().enumerate() {
+                let en = st.add_days(*days);
+                assert_eq!(en, *expected_date, "case #{} (0-based) failed", i);
+            }
         }
     }
 
     mod date_subtract {
         use super::*;
         #[test]
-        fn test_one_day_subtraction() {
-            let term1 = Date {
-                year: 2025,
-                month: 12,
-                day: 30,
-            };
-            let term2 = Date {
-                year: 2025,
-                month: 12,
-                day: 29,
-            };
+        fn test_regular_year_duration() {
+            let st = Date::new::<2001, 1, 1>();
+            let en = Date::new::<2001, 12, 31>();
+            let diff = en.subtract(&st);
+            assert_eq!(diff, 364);
+        }
 
+        #[test]
+        fn test_leap_year_duration() {
+            let st = Date::new::<2000, 1, 1>();
+            let en = Date::new::<2000, 12, 31>();
+            let diff = en.subtract(&st);
+            assert_eq!(diff, 365);
+        }
+
+        #[test]
+        fn test_one_day_subtraction() {
+            let term1 = Date::new::<2025, 12, 30>();
+            let term2 = Date::new::<2025, 12, 29>();
             let diff = term1.subtract(&term2);
             assert_eq!(diff, 1)
         }
 
         #[test]
         fn test_cross_leap_year() {
-            let week_start = Date {
-                year: 2028,
-                month: 12,
-                day: 29,
-            };
-            let event_start = Date {
-                year: 2029,
-                month: 1,
-                day: 4,
-            };
+            let week_start = Date::new::<2028, 12, 29>();
+            let event_start = Date::new::<2029, 1, 4>();
             let diff = event_start.subtract(&week_start);
             assert_eq!(diff, 6)
         }
 
         #[test]
         fn test_cross_year() {
-            let week_start = Date {
-                year: 2025,
-                month: 12,
-                day: 29,
-            };
-            let event_start = Date {
-                year: 2026,
-                month: 1,
-                day: 4,
-            };
+            let week_start = Date::new::<2025, 12, 29>();
+            let event_start = Date::new::<2026, 1, 4>();
             let diff = event_start.subtract(&week_start);
             assert_eq!(diff, 6)
         }
