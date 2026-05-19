1301259eab4a5debdcfbbde22647a037593d92c5
Aleksei Markov <laladrik@pm.me>
2026-05-19 20:02:36 +0100

    chore(application): refactoring

--- crates/application/src/state.rs
+++ crates/application/src/state.rs
@@ -1,4 +1,8 @@
 use crate::render::{EventViewRenderData, RenderData, WeekViewRenderData};
+mod calendar_state;
+use calendar_state::CalendarState;
+use calendar_state::WeekData;
+use calendar_state::EventRectangles;
 
 use calendar::{
     date::DateStream,
@@ -13,95 +17,6 @@ pub struct Calendar<F: Frontend> {
     pub week_start: calendar::date::Date,
     pub is_week_switched: bool,
     state: CalendarState<<F::AgendaSource as AgendaSource>::RequestHandle>,
-}
-
-enum CalendarState<Handle> {
-    Loading {
-        agenda_source_handle: Handle,
-    },
-    Ready {
-        week_data: WeekData,
-        long_event_clash_size: calendar::Lane,
-        short_event_rectangles_opt: calendar::render::Rectangles,
-        long_event_rectangles_opt: calendar::render::Rectangles,
-    },
-    Rendering {
-        week_data: WeekData,
-        long_event_clash_size: calendar::Lane,
-    },
-}
-
-impl<H> CalendarState<H> {
-    fn obtain_events<'a>(&'a self) -> EventRectangles<'a> {
-        match self {
-            Self::Loading { .. } => EventRectangles {
-                long: &NO_RECT,
-                short: &NO_RECT,
-            },
-            Self::Ready {
-                short_event_rectangles_opt,
-                long_event_rectangles_opt,
-                ..
-            } => EventRectangles {
-                long: long_event_rectangles_opt,
-                short: short_event_rectangles_opt,
-            },
-            Self::Rendering { .. } => {
-                unreachable!("unexpected state of the calendar")
-            }
-        }
-    }
-
-    fn obtain_long_events_titles(&self) -> &[impl AsRef<str>] {
-        if let Self::Ready { week_data, .. } = self {
-            week_data.agenda.long.titles.as_slice()
-        } else {
-            NO_TITLES.as_slice()
-        }
-    }
-
-    fn obtain_short_events_titles(&self) -> &[impl AsRef<str>] {
-        if let Self::Ready { week_data, .. } = self {
-            week_data.agenda.short.titles.as_slice()
-        } else {
-            NO_TITLES.as_slice()
-        }
-    }
-
-    /// It provides a memory-safe way to switch the state.  The function creates an uninitialized
-    /// state to replace the current one.  Then it tries to switch to the next state provided by
-    /// the function `update`.  The function must return any valid state and an error if any has
-    /// occurred.
-    fn switch<E>(&mut self, update: impl FnOnce(Self) -> (Self, Option<E>)) -> Result<(), E> {
-        // SAFETY: the bald_state is never read until the function finishes.
-        let bald_state: CalendarState<_> = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
-        let current_state = std::mem::replace(self, bald_state);
-        let (new_state, error) = update(current_state);
-        *self = new_state;
-        match error {
-            Some(e) => Err(e),
-            None => Ok(()),
-        }
-    }
-
-    fn switch_infallible(&mut self, update: impl Fn(Self) -> Self) {
-        // SAFETY: the bald_state is never read until the function finishes.
-        // FIXME(alex): the state is 200 bytes long.
-        let bald_state: CalendarState<_> = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
-        let current_state = std::mem::replace(self, bald_state);
-        let new_state = update(current_state);
-        *self = new_state;
-    }
-
-    /// a shortcut to switch to the [`CalendarState<H>::Loading`]
-    fn loading<E>(agenda_source_handle: H, e: Option<E>) -> (Self, Option<E>) {
-        (
-            Self::Loading {
-                agenda_source_handle,
-            },
-            e,
-        )
-    }
 }
 
 impl<F: Frontend> Calendar<F> {
@@ -173,7 +88,7 @@ impl<F: Frontend> Calendar<F> {
         });
     }
 
-    // transition from rendering to ready
+    // transition from the state "rendering" to the state "ready"
     fn get_ready<'ttc>(
         &mut self,
         view: &View,
@@ -254,14 +169,6 @@ impl<F: Frontend> Calendar<F> {
             CalendarState::Ready { .. } | CalendarState::Rendering { .. } => false,
         }
     }
-}
-
-static NO_RECT: calendar::render::Rectangles = Vec::new();
-static NO_TITLES: Vec<String> = Vec::new();
-
-struct EventRectangles<'rect> {
-    long: &'rect calendar::render::Rectangles,
-    short: &'rect calendar::render::Rectangles,
 }
 
 pub struct UserInterface {
@@ -1230,11 +1137,6 @@ pub trait AgendaSource {
         handle: &Self::RequestHandle,
         week_start: &calendar::date::Date,
     ) -> calendar::obtain::WeekScheduleWithLanes;
-}
-
-// FIXME: Flatten the structure
-pub struct WeekData {
-    pub agenda: calendar::obtain::WeekScheduleWithLanes,
 }
 
 /// Stores textures of the text objects.
